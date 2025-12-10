93b12ea649d32c235ce1221ec0ac563a53ab8415
Guillaume <gbeuriot.95@gmail.com>
2025-12-10 16:34:10 +0100

    schema for addresses changed and routes relating to address modifcation changed

--- models/users.js
+++ models/users.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
+const addressesSchema = mongoose.Schema({
+  address: String,
+  coords: Array,
+});
+
 const userSchema = mongoose.Schema({
   email: String,
   password: String,
   username: String,
   token: String,
-  addresses: [String],
+  addresses: [addressesSchema],
 });
 
 const User = mongoose.model("users", userSchema);
