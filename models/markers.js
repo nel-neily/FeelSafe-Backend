167919a18f610cfcf3d69f0fbebb79d6c2d5630c
Nel <nel.boudjelal@gmail.com>
2025-12-18 15:05:04 +0100

    restriction levée sur le user id pour création de marker

--- models/markers.js
+++ models/markers.js
@@ -18,7 +18,7 @@ const markerSchema = new mongoose.Schema({
   color: String,
   riskType: String,
   upvotes: Number,
-  users: { type: mongoose.Schema.Types.ObjectId, ref: "users" } | null, // référence à l'utilisateur
+  users: { type: mongoose.Schema.Types.ObjectId, ref: "users", default: null }, // référence à l'utilisateur
   createdAt: { type: Date, default: Date.now() }, // date de création,
   polygon: polygonSchema,
 });
